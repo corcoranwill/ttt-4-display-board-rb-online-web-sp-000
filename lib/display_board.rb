645108ccfb14ff6aad5966df1079726297da4acf
Will Corcoran <corcoranwill@gmail.com>
2020-02-02 12:11:29 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+# def display_board(board_array)
+  # read board_array
+  # print out a multi-line board 
+# end
+
+
+
+
+
+# def board_array = ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "]
